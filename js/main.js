12670a4de2c295f3a6e4e939826b655d3977ce5a
Nikita <nickermen13@gmail.com>
2019-05-01 19:27:43 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function($){
+	$(document).ready(function() {
+		// Code
+		var plus = $('<div class="plus"></div>'),
+			triang = $('<div class="triang"></div>');
+
+		$('.slider-wrapper').append(plus, triang)
+
+
+		$('.slider').slick({
+			autoplay: true,
+  			autoplaySpeed: 3000,
+			variableWidth: true,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			arrows: true,
+			adaptiveHeight: false,
+			responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+			]
+		});
+
+
+	});
+})(jQuery);
